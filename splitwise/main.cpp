9a02deaa9999f9472f1d29e4ead23744111e4e36
Apurva220998 <apurvamirashi@gmail.com>
2026-04-12 14:34:03 +0000

    splitwise changes

--- splitwise/main.cpp
+++ splitwise/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std; 
 class user{
     int id;
@@ -48,15 +49,17 @@ class split{
    
 class splitType{
     public:
-    virtual void splitAmount(double amount, vector<user*> users, vector<split>& splits) = 0;
+    virtual void splitAmount(double amount,user* from, vector<user*> users, vector<split>& splits) = 0;
     virtual ~splitType() {}
 };
 class equalSplit : public splitType{
-    void splitAmount(double amount, vector<user*> users, vector<split>& splits ) override {
+    void splitAmount(double amount, user* from, vector<user*> users, vector<split>& splits ) override {
         double splitAmount = amount / users.size();
         for (auto user : users) {
-            splits.push_back(split(splits.size() + 1, splitAmount, user, nullptr));
-            cout << user->getName() << " owes " << splitAmount << endl;
+            if (user != from) {
+                splits.push_back(split(splits.size() + 1, splitAmount, user, from));
+                cout << user->getName() << " owes " << splitAmount << endl;
+            }
         }
     }
 };
@@ -64,10 +67,10 @@ class percentageSplit : public splitType{
     vector<double> percentages;
     public:
     percentageSplit(vector<double> percentages): percentages(percentages) {}
-    void splitAmount(double amount, vector<user*> users, vector<split>& splits) override {
+    void splitAmount(double amount, user* from, vector<user*> users, vector<split>& splits) override {
         for (size_t i = 0; i < users.size(); i++) {
             double splitAmount = amount * (percentages[i] / 100);
-            splits.push_back(split(splits.size() + 1, splitAmount, users[i], nullptr));     
+            splits.push_back(split(splits.size() + 1, splitAmount, users[i], from));     
             cout << users[i]->getName() << " owes " << splitAmount << endl;
         }
     }
@@ -75,14 +78,47 @@ class percentageSplit : public splitType{
 
 class transaction{
 public:
-    string from;
+    user* from;
     vector<user*> to;
     splitType* type;
     vector<split> splits;
-    transaction(string from, vector<user*> to, splitType* type): from(from), to(to), type(type) {}
-    void addTransaction(double amount) {
-        type->splitAmount(amount, to, splits);
+    double amount;
+    transaction(user* from, double amount, vector<user*> to, splitType* type): from(from), amount(amount), to(to), type(type) {}
+    void addTransaction() {
+        type->splitAmount(amount,from, to, splits);
     }
+    vector<split> getSplits() const {
+        return splits;
+    }
+};
+
+class balance{
+    unordered_map<user*, unordered_map<user*, double>> balances;
+public:
+    void updateBalance(split split) {
+        user* owed = split.getOwed();
+        user* paidBy = split.getPaidBy();
+        double amount = split.getAmount();
+        balances[owed][paidBy] += amount;
+        balances[paidBy][owed] -= amount;
+    }
+    void showBalance(user* user) {
+        cout << "Balance for " << user->getName() << ":" << endl;
+        for (const auto& entry : balances[user]) {
+            cout << "  Owes " << entry.first->getName() << ": " << entry.second << endl;
+        }
+};
+
+class splitwise{
+    unordered_map<user*, vector<transaction>> transactions;
+    void createTransaction(user* from, vector<user*> to, splitType* type, double amount) {
+        transaction t(from,amount,to, type);
+        t.addTransaction();
+        transactions[from].push_back(t);
+    }
+  
+
+
 };
 int main() {
     user* user1 = new user(1, "Alice");
@@ -90,11 +126,11 @@ int main() {
     user* user3 = new user(3, "Charlie");
     vector<user*> users = {user1, user2, user3};
     splitType* equal = new equalSplit();
-    transaction t1("Alice", users, equal);
-    t1.addTransaction(300);
+    transaction t1(user1,300, users, equal);
+    t1.addTransaction();
     splitType* percentage = new percentageSplit({50, 30, 20});
-    transaction t2("Bob", users, percentage);
-    t2.addTransaction(200);
+    transaction t2(user1,200, users, percentage);
+    t2.addTransaction();
     delete user1;
     delete user2;
     delete user3;
